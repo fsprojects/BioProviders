6b4d0f405af47957faf8ba46d354385f7aee608b
AlexKenna <AlexKenna04@gmail.com>
2022-02-01 12:48:40 +1000

    Updated BaseTypes for RunTime.

--- src/RunTime/BaseTypes.fs
+++ src/RunTime/BaseTypes.fs
@@ -1,14 +1,14 @@
-﻿// --------------------------------------------------------------------------------------
-// Base RunTime Types for Type Provider Erasure.
-// --------------------------------------------------------------------------------------
-
-namespace BioProviders.RunTime
+﻿namespace BioProviders.RunTime
 
 open System.IO
 open Bio.IO.GenBank
 open BioProviders.Common
 
-type GenomicGenBankFlatFile (path:string) = 
+// --------------------------------------------------------------------------------------
+// Base RunTime Types Genomic GenBank Flat File.
+// --------------------------------------------------------------------------------------
+
+type GenomicGBFF (path:string) = 
 
     let cache = new Cache()
 
@@ -19,43 +19,26 @@ type GenomicGenBankFlatFile (path:string) =
         |> Seq.cast<Bio.ISequence>
         |> Seq.head
 
-    member _.Metadata with get() = new GenBankGenomicMetadata(sequence)
+    member _.Metadata with get() = new GenomicGBFFMetadata(sequence)
     
-    
-and GenBankGenomicMetadata (sequence:Bio.ISequence) = 
+and GenomicGBFFMetadata (sequence:Bio.ISequence) = 
     
     let metadata = ( sequence.Metadata.Item("GenBank") :?> GenBankMetadata )
     
     member _.Accession with get() = metadata.Accession
-
     member _.BaseCount with get() = metadata.BaseCount
-
     member _.Comments with get() = metadata.Comments
-
     member _.Contig with get() = metadata.Contig
-
     member _.DbLinks with get() = metadata.DbLinks
-
     member _.DbSource with get() = metadata.DbSource
-
     member _.Definition with get() = metadata.Definition
-
     member _.Features with get() = metadata.Features
-
     member _.Keywords with get() = metadata.Keywords
-
     member _.Locus with get() = metadata.Locus
-
     member _.Origin with get() = metadata.Origin
-
     member _.Primary with get() = metadata.Primary
-
     member _.Project with get() = metadata.Project
-
     member _.References with get() = metadata.References
-
     member _.Segment with get() = metadata.Segment
-
     member _.Source with get() = metadata.Source
-
     member _.Version with get() = metadata.Version
