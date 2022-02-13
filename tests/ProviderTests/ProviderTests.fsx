5326d6295a25bfcc9ac429f217e4422a53b6e473
AlexKenna <AlexKenna04@gmail.com>
2022-02-13 13:12:36 +1000

    Updated testing script to include examples for a single assembly and multiple assemblies.

--- tests/ProviderTests/ProviderTests.fsx
+++ tests/ProviderTests/ProviderTests.fsx
@@ -1,10 +1,30 @@
 #I "../../bin/Debug/netstandard2.0"
-#r "FSharp.Data.BioProviders.dll"
+#r "Bio.Core.dll"
+#r "FSharp.Data.dll"
 
-open GenBank
+open BioProviders
 
-type AssemblyType = AssemblyProvider<"bacteria", "Staphylococcus_borealis", "GCA_003042555.1_ASM304255v1">
+//-------------------------------------------------------------//
+//  Creating a type for a single Assembly.                     //
+//-------------------------------------------------------------//
 
-let genome = AssemblyType.GenomicGBFF()
+let [<Literal>] taxon = "bacteria"
+let [<Literal>] species = "Staphylococcus_borealis"
+let [<Literal>] assembly = "GCA_003042555.1_ASM304255v1"
 
-genome.Metadata.Accession
+type Assembly304255Type = GenBankProvider<taxon, species, assembly>
+
+let genome304255 = Assembly304255Type.GenomicGBFF()
+
+//-------------------------------------------------------------//
+//  Creating a type for multiple Assemblies.                   //
+//-------------------------------------------------------------//
+
+let [<Literal>] assemblyWildcard = "GCA*"
+
+type SpeciesType = GenBankProvider<taxon, species, assemblyWildcard>
+
+type Assembly358083Type = SpeciesType.``GCA_003580835.1_ASM358083v1``
+
+let genome358083 = Assembly358083Type.GenomicGBFF()
+
