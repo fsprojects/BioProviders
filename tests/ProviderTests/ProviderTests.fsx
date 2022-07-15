9ab6a977a24ed58ed1e16aa2e0c097f5bdb81fb9
AlexKenna <AlexKenna04@gmail.com>
2022-07-15 10:41:02 +1000

    Updated to net6.0 and FSharp 6.0.5

---
+++ tests/ProviderTests/ProviderTests.fsx
@@ -0,0 +1,19 @@
+﻿#I "../../bin/Debug/lib/netstandard2.0"
+#r "BioProviders.dll"
+
+open BioProviders
+
+// Generate the Assembly Type
+let [<Literal>] Taxon = "bacteria"
+let [<Literal>] Species = "Staphylococcus_lugdunensis"
+let [<Literal>] Assembly = "GCA_000185485.1_ASM18548v1"
+
+type AssemblyType = GenBankProvider<Taxon, Species, Assembly>
+
+
+// Use the Assembly Type
+let gbff = AssemblyType.Genome()
+
+gbff.Sequence.GetSubSequence 0 20
+|> fun x -> x.ToString()
+|> printfn "The first 20 bases for the sequence are: %s"
