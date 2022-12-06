c22cae061c4e5d3cfaea27451e6f24ca24deaa7f
AlexKenna <AlexKenna04@gmail.com>
2022-12-07 09:45:14 +1000

    Added basic helper functions for project

---
+++ src/Common/Helpers.fs
@@ -0,0 +1,13 @@
+﻿namespace BioProviders.Common
+
+// --------------------------------------------------------------------------------------
+// Project Helpers.
+// --------------------------------------------------------------------------------------
+
+module Helpers = 
+
+    // Optional String
+    let parseOptionString (str:string) =
+        match System.String.IsNullOrEmpty(str) with
+        | true -> None
+        | _ -> Some str
